f2e14474e93b7c4e0f0156e9026c916e10b749ad
Hiran Hansaka <69340343+Hiran117@users.noreply.github.com>
2023-08-24 11:13:11 +0530

    create tables using entity frame work core

---
+++ Bloggie.web/Data/BloggieDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bloggie.web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.web.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        //create tables using entity frame work core
+        public DbSet<BlogPost> blogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
