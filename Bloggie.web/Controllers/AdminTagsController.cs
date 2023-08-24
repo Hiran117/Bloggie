b9169a7a39ecde349bdd1e070659e43c13d9185f
Hiran Hansaka <69340343+Hiran117@users.noreply.github.com>
2023-08-24 14:43:50 +0530

    save data to db

--- Bloggie.web/Controllers/AdminTagsController.cs
+++ Bloggie.web/Controllers/AdminTagsController.cs
@@ -1,10 +1,19 @@
-﻿using Bloggie.web.Models.ViewModels;
+﻿using Bloggie.web.Data;
+using Bloggie.web.Models.Domain;
+using Bloggie.web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -14,9 +23,17 @@ namespace Bloggie.web.Controllers
         [HttpPost]
         [ActionName("Add")]
         public IActionResult Add(AddTagRequest addTagRequest)
-        {      
-            var name = addTagRequest.Name;
-            var displayName = addTagRequest.DisplayName;
+        {
+            //Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+
+         bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
             return View("Add");
         }
     }
