c862a9dfcc0f0d7ac0850189b56557d9964b326d
Hiran Hansaka <69340343+Hiran117@users.noreply.github.com>
2023-08-24 11:29:02 +0530

    inject Dbcontext

--- Bloggie.web/Program.cs
+++ Bloggie.web/Program.cs
@@ -1,7 +1,14 @@
+using Bloggie.web.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//inject Dbcontext
+builder.Services.AddDbContext<BloggieDbContext>(Options => Options.UseSqlServer(builder.Configuration.GetConnectionString("BloggieDbConnectionString")));
 
 var app = builder.Build();
 
