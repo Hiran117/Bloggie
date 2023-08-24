3877ca893e209b416854675856d07c07df1ee57f
Hiran Hansaka <69340343+Hiran117@users.noreply.github.com>
2023-08-24 18:24:03 +0530

    Create New Controller (AdminBlogs), Add Action Method (GET) and Create New View Create New HTML Form to Add new Blog

---
+++ Bloggie.web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,31 @@
+﻿using Bloggie.web.Models.ViewModels;
+using Bloggie.web.Repositories;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.web.Controllers
+{
+	public class AdminBlogPostsController : Controller
+	{
+		private readonly ITagRepository tagRepository;
+
+		public AdminBlogPostsController(ITagRepository tagRepository)
+		{
+			this.tagRepository = tagRepository;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Add()
+		{
+			//get tags from repository
+			var tags = await tagRepository.GetAllAsync();
+
+			var model = new AddBlogPostRequest
+			{
+				Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() })
+			};
+
+			return View(model);
+		}
+	}
+}
